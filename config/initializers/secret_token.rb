22a6e5fc33b29c06a869150aa853f25c8afac857
airdisa <disa@searchreturn.com>
2013-08-16 15:15:49 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hearus::Application.config.secret_token = '5fefb15e2a0a0354e4fabedc1d61f56e79dfe87d58752d5adb3fa9c7af5e63eb31c5861dc217785c2e554cab09f7ed339be8200c3d940883b5ddc60274444fc6'
