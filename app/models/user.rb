d127015dbf1323eeabaf5015f9d64d77c16a5847
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-17 16:34:17 -0500

    update user model with zipcode and coordinate methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
                   :last_name,
                   :password,
                   :username
-  # after_update :set_district, :if => :district_id_changed?
+  # after_update :set_representative, :if => :district_id_changed?
 
   validates_presence_of :email,
                         :password
@@ -19,6 +19,10 @@ class User < ActiveRecord::Base
 
   end
 
+  def get_zipcode(zipcode)
+    set_district_by_zipcode(zipcode.strip.to_i)
+  end
+
   def get_coordinates(address)
     coordinates = Geocoder.coordinates(address)
     self.latitude = coordinates[0]
@@ -28,9 +32,25 @@ class User < ActiveRecord::Base
   end
   protected
 
-  def set_district
+  def set_district_by_zipcode(zipcode)
+    legislators = Sunlight::Legislator.all_in_zipcode(zipcode)
+    if legislators.length == 3
+      legislators.each do |leg|
+        if leg.title == "Rep"
+          self.district = District.where("state_name = ? AND number = ?", leg.state, leg.district.to_i )[0]
+          self.save
+          set_representative
+        end
+      end
+    true
+   else
+    false
+   end
+  end
+
+  def set_district_by_coordinates
     district = Sunlight::District.get(:latitude => self.latitude,
-                           :longitude => self.longitude)
+                                      :longitude => self.longitude)
     self.district = District.find_by_number(district.number.to_i)
     self.state = district.state
     self.district_number = district.number.to_i
@@ -40,10 +60,6 @@ class User < ActiveRecord::Base
 
   def set_representative
     self.rep_name = self.district.rep_name
-    self.rep_twitter = self.district.rep_twitter
-    self.rep_phone = self.district.rep_phone
-    self.rep_email_form = self.district.rep_email_form
-
     self.rep_email_form = self.district.rep_email_form
     self.rep_party = self.district.rep_party
     self.rep_phone = self.district.rep_phone
