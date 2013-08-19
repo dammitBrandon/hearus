235d1aafed608b3663629ba1bb913521c0f2a1ad
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-19 16:54:50 -0500

    User can now on bills

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,15 @@
-class VoteController < ApplicationController.rb
+class VotesController < ApplicationController
   def create
     @vote = Vote.new(
       :choice => params[:vote],
-      :user_id => current_user.id,
+      :user_id => params[:user_id],
       :bill_id => params[:bill_id])
     if @vote.save
-      redirect_to vote(@vote)
+      redirect_to vote_path(@vote)
     else
       @errors = @vote.errors.full_messages
       # redirect_to bill_path (unique_bill_id: params[:unique_bill_id], errors: @errors)
-      redirect_to bill(params[:bill_id])
+      redirect_to bill_path(params[:bill_id])
     end
   end
 
