7a7839f8ede89941728b52cc16ab33994c952b7f
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-08-18 20:08:12 -0500

    fixing bugs

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,6 +3,9 @@ class LandingController < ApplicationController
     if current_user? && current_district?
       redirect_to district_path
     end
+    puts "*"*100
+    p current_user
+    @user = current_user
   end
 
   def about
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      session[:district_id] = user.district.id
-      if session[:district_id]
-        redirect_to district_path
-      else
-        redirect_to root_path
-      end
-    else
-      render "new"
-    end
-
     if request.env["omniauth.auth"].present?
 
       oauth = OAuthUser.new(request.env["omniauth.auth"], current_user)
@@ -27,8 +14,9 @@ class SessionsController < ApplicationController
       user = RegularUser.find_by_email(params[:user][:email])
       if user && user.authenticate(params[:user][:password])
         session[:user_id]    = user.id
+        session[:district_id] = user.district.id
+        redirect_to district_path if session[:district_id]
         redirect_to root_path
-
       else
         @error = "Invalid Login"
         render action: 'new'
