b5e33fed2e22cbf438fc5dd7c1073d4b3eb84efe
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-08-20 09:36:04 -0500

    cleaning up models and migrations

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :user do
+    id '21'
+    first_name 'Gerrod'
+    last_name 'Wilson'
+    username 'GRod'
+    email 'Gerrod.wilson@three_courts.com'
+    password 'password'
+    password_confirmation 'password'
+    address = '851 Windward Court, Fort Washington, Md, 20744'
+    latitude, longitude = Geocoder.coordinates(address)
+    district = Sunlight::District.get(latitude: latitude, longitude: longitude)
+    district_number = district.number
+  end
+end
