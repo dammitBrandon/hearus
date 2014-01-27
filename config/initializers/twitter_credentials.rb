2559c817a673d0dd833f5fb91fa444040c43e8e9
btbailey <brandon.bailey@slalom.com>
2014-01-26 19:34:20 -0600

    updated initializers/twitter_credentials.rb for combatibility with v5

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,6 +1,11 @@
 require 'twitter'
 
-Twitter.configure do |config|
+#Twitter.configure do |config|
+#  config.consumer_key = ENV['HEARUS_TWITTER_ID']
+#  config.consumer_secret = ENV['HEARUS_TWITTER_SECRET']
+#end
+
+client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['HEARUS_TWITTER_ID']
   config.consumer_secret = ENV['HEARUS_TWITTER_SECRET']
 end
